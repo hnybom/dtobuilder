03b947a1f281578c340bd670d9e34458bbba0a56
henri.nybom@gmail.com <henri.nybom@gmail.com@370c5c53-eb9a-7bc7-57af-21b5c6bf71cb>
2012-03-06 08:57:28 +0000

    Tidying up

--- trunk/DTOBuilder/src/main/java/org/kahvi/paketti/dtobuilder/util/DtoUtil.java
+++ trunk/DTOBuilder/src/main/java/org/kahvi/paketti/dtobuilder/util/DtoUtil.java
@@ -1,9 +1,14 @@
 package org.kahvi.paketti.dtobuilder.util;
 
 import java.lang.reflect.Method;
+import org.kahvi.paketti.dtobuilder.annotations.DtoProperty;
 
 public class DtoUtil {
 	
+    public static boolean isPropertyNameGiven(final DtoProperty dtoProperty) {
+        return !"".equals(dtoProperty.sourceProperty());
+    }
+    
 	public static String capitalize(String str) {
 		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
 	}
